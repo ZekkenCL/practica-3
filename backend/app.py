59b0ead4810f472eeb061c8286880cdc1e08d03c
ZekkenCL <benito.munoz@alumnos.ucn.cl>
2023-12-06 15:01:54 -0300

    feat: mobile

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from extensions import db, migrate
 from models import User, Framework, Hobby
 from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://"USUARIO":"CONTRASEÑA"@localhost:3306/practica2'
@@ -14,6 +15,39 @@ CORS(app)
 def get_profile():
     user = User.query.first()
     return jsonify(user.to_dict())
+
+@app.route('/api/profile/<rut>', methods=['PUT'])
+def update_profile(rut):
+    user = User.query.filter_by(rut=rut).first()
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+
+    data = request.get_json()
+
+    # Actualizar los campos del usuario
+    user.name = data.get('name', user.name)
+    user.email = data.get('email', user.email)
+    user.city = data.get('city', user.city)
+    user.country = data.get('country', user.country)
+    user.summary = data.get('summary', user.summary)
+
+    # Actualizar los frameworks
+    if 'frameworks' in data:
+        Framework.query.filter_by(user_rut=user.rut).delete()
+        for framework_data in data['frameworks']:
+            framework = Framework(name=framework_data['name'], level=framework_data['level'], user_rut=user.rut)
+            db.session.add(framework)
+
+    # Actualizar los hobbies
+    if 'hobbies' in data:
+        Hobby.query.filter_by(user_rut=user.rut).delete()
+        for hobby_data in data['hobbies']:
+            hobby = Hobby(name=hobby_data['name'], user_rut=user.rut)
+            db.session.add(hobby)
+
+    db.session.commit()
+    return jsonify(user.to_dict()), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
